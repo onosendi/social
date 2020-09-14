0c2135594be664d81601e80cc41e4fb11c90db62
Daniel Lindegren <onosendi@protonmail.com>
2020-09-14 09:00:24 -0700

    Route users to next=... at login

--- frontend/static/frontend/src/components/AuthRoute/AuthRoute.jsx
+++ frontend/static/frontend/src/components/AuthRoute/AuthRoute.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Redirect, Route } from 'react-router-dom';
+import { Redirect, Route, useLocation } from 'react-router-dom';
 
 import { route } from '../../constants';
 
@@ -11,13 +11,15 @@ const AuthRoute = ({
   component: Component,
   ...rest
 }) => {
+  const location = useLocation();
+
   const user = useSelector(selectUser);
 
   return (
     <Route
       render={(props) => (
         !user.username
-          ? <Redirect to={route.login} />
+          ? <Redirect to={`${route.login}?next=${location.pathname}`} />
           // eslint-disable-next-line react/jsx-props-no-spreading
           : <Component {...props} />
       )}
