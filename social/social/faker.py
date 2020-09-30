d27af49b83836ca6816da0a09059c46b8b6c26ca
Daniel Lindegren <onosendi@protonmail.com>
2020-09-30 11:36:10 -0700

    faker: include sex when creating users

--- social/social/faker.py
+++ social/social/faker.py
@@ -1,8 +1,10 @@
+# import os
 import datetime
 from random import randint, randrange
 
 from faker import Faker
 
+# from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.db import IntegrityError, transaction
 from django.utils import timezone
@@ -11,73 +13,47 @@ from posts.models import Post
 from users.models import Profile
 
 
-explicit_users = [
-    ('Jim', 'Fowler'),
-    ('Lucy', 'McDonald'),
-    ('John', 'Cummings'),
-]
 faker = Faker()
 User = get_user_model()
 
 
-def _create_user(
-    first: str = None,
-    last: str = None,
-) -> None:
-    ''' Create the user.
-
-    If the user exists in the database, integers will keep getting appended to
-    the user's username until no error is raised. This ensures the correct
-    number of users are being added from the given ``count`` parameter
-    in :func:`create_users`.
-
-    :param first: First name of user to create.
-    :param last: Last name of user to create.
-    '''
-    # Set password to ``None`` if no first name and no last name is given. This
-    # means the user is random, and not explicit. We do not want to hash random
-    # users' passwords to save time.
-    password = 'testing' if first is not None and last is not None else None
-    if first is None:
-        first = faker.first_name()
-    if last is None:
-        last = faker.last_name()
-    username = first.lower()
-    while True:
-        try:
-            with transaction.atomic():
-                email = f'{username}@testing.com'
-                user = User.objects.create_user(
-                    name=f'{first} {last}',
-                    username=username,
-                    email=email,
-                    password=password,
-                )
-                profile_data = {
-                    'bio': faker.company(),
-                    'location': f'{faker.city()}, {faker.state()}',
-                }
-                Profile.objects.filter(user_id=user.id).update(**profile_data)
-        except IntegrityError:
-            random_number = randint(0, 9)
-            username = f'{username}{random_number}'
-        else:
-            break
-
-
 def create_users(count: int = 100) -> None:
-    ''' Create explicit and random users.
+    def get_sex():
+        sex = ['M', 'F']
+        return sex[randrange(2)]
 
-    The length of :var:`explicit_users` is subtracted from ``count`` so the
-    correct number of users will be added.
-
-    :param count: Number of random users to create.
-    '''
-    for first, last in explicit_users:
-        _create_user(first, last)
-    count = count - len(explicit_users)
     for _ in range(count):
-        _create_user()
+        sex = get_sex()
+        if sex == 'M':
+            first = faker.first_name_male()
+        else:
+            first = faker.first_name_female()
+        last = faker.last_name()
+        password = None
+        username = first.lower()
+        while True:
+            try:
+                with transaction.atomic():
+                    email = f'{username}@testing.com'
+                    user = User.objects.create_user(
+                        name=f'{first} {last}',
+                        username=username,
+                        email=email,
+                        password=password,
+                    )
+                    profile_data = {
+                        'bio': faker.company(),
+                        'location': f'{faker.city()}, {faker.state()}',
+                        'sex': sex,
+                    }
+                    Profile.objects\
+                        .filter(user_id=user.id)\
+                        .update(**profile_data)
+            except IntegrityError:
+                random_number = randint(0, 9)
+                username = f'{username}{random_number}'
+            else:
+                break
 
 
 def create_posts():
@@ -160,3 +136,9 @@ def randomize_timestamps():
         new_date = new_date.replace(tzinfo=timezone.get_default_timezone())
         post.created_at = new_date
         post.save()
+
+
+# def set_images():
+#     base_dir = settings.BASE_DIR
+#     male_img_dir = os.path.join(base_dir, 'media/fake/male')
+#     female_img_dir = os.path.join(base_dir, 'media/fake/female')
